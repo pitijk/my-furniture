9ebc1382ec269766da9205a624b163e619363b70
Piotr Kielek <piotrek4kontak@gmail.com>
2019-10-28 16:11:12 +0100

    initial cleanup

---
+++ src/reducers/furnitures.js
@@ -0,0 +1,8 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case "type":
+      return;
+    default:
+      return state;
+  }
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import furnitures from "./furnitures";
+
+export default combineReducers({
+  furnitures
+});
