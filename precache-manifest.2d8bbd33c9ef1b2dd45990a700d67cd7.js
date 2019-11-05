7157161ddd0f6b5b3b3cacc539d50055461b675e
Piotr Kielek <piotrek4kontak@gmail.com>
2019-11-05 13:37:57 +0100

    Updates

---
+++ precache-manifest.2d8bbd33c9ef1b2dd45990a700d67cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4093ce23c8f6e82065652b5fc28d5d99",
+    "url": "/my-furnitures/index.html"
+  },
+  {
+    "revision": "8977a425b0b8198a0c8e",
+    "url": "/my-furnitures/static/css/main.d10686e0.chunk.css"
+  },
+  {
+    "revision": "adb102fb32030f3632a5",
+    "url": "/my-furnitures/static/js/2.3faa08a6.chunk.js"
+  },
+  {
+    "revision": "8977a425b0b8198a0c8e",
+    "url": "/my-furnitures/static/js/main.98911a6e.chunk.js"
+  },
+  {
+    "revision": "2e2397fd803edc87d93a",
+    "url": "/my-furnitures/static/js/runtime-main.cac27fd7.js"
+  }
+]);
